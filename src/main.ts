4ecfab8f4c05a4bf79614096b45c5e8cca311130
JOAO <joao.abbasi@gmail.com>
2022-05-27 10:29:01 -0300

    🏁 feat: transform interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './transform.interceptor';
 
 const env = require('ckey'); // ckey is used here to access .env file.
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(env.APP_PORT);
   console.log('🚀 App started.');
 }
